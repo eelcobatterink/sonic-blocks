812c1d19437ec7dfa5ec3d8875031d3c15457fa6
Eelco Batterink <Eelco Batterink>
2017-01-09 16:06:43 +0000

    Add simple stand-alone example

---
+++ eelco_example/sonic.js
@@ -0,0 +1,62 @@
+// JavaScript source code
+
+Blockly.Blocks['sonic_pi_play'] = {
+    /**
+     * Block for repeat n times (external number).
+     * @this Blockly.Block
+     */
+    init: function () {
+        this.jsonInit({
+            "type": "sonic_pi_play",
+            "message0": "Play %1",
+            "args0": [
+              {
+                  "type": "input_value",
+                  "name": "Play",
+              }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": 230,
+            "tooltip": "Play a note",
+            "helpUrl": ""
+        });
+    }
+};
+
+Blockly.Python['sonic_pi_play'] = function (block) {
+    var value_play = Blockly.Python.valueToCode(block, 'Play', Blockly.Python.ORDER_ATOMIC);
+    return 'play ' + value_play;
+};
+
+
+Blockly.Blocks['sonic_pi_sleep'] = {
+    /**
+     * Block for repeat n times (external number).
+     * @this Blockly.Block
+     */
+    init: function () {
+        this.jsonInit({
+            "type": "sonic_pi_play",
+            "message0": "Sleep %1",
+            "args0": [
+              {
+                  "type": "input_value",
+                  "name": "Sleep",
+              }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": 230,
+            "tooltip": "Play a note",
+            "helpUrl": ""
+        });
+    }
+};
+
+Blockly.Python['sonic_pi_sleep'] = function (block) {
+    var value_play = Blockly.Python.valueToCode(block, 'Sleep', Blockly.Python.ORDER_ATOMIC);
+    return 'play ' + value_play + '\n';
+};
