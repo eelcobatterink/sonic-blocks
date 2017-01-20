9970bdc6c757f5d08b157bf359f81e6420a8e944
Eelco Batterink <Eelco Batterink>
2017-01-20 10:49:39 +0000

    merge blocklib with eelco_example and add new blocks

---
+++ eelco_example/play.js
@@ -0,0 +1,143 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.play');
+goog.require('Blockly.Blocks');
+
+
+Blockly.Blocks['play_basic'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("play");
+    this.appendValueInput("NOTE")
+        .setCheck(["note", "midi-note"]);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['midi_note'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("note number")
+        .appendField(new Blockly.FieldNumber(72, 0, 127), "NOTE_NUMBER");
+    this.setOutput(true, "midi-note");
+    this.setColour(135);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['note'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["C", ":C"], ["C#/Db", ":Cs"], ["D", ":D"], ["D#/Eb", ":Ds"], ["E", ":E"], ["F", ":F"], ["F#", ":Fs"], ["G", ":G"], ["G#", ":Gs"], ["A", ":A"], ["A#", ":As"], ["B", ":B"]]), "NOTE")
+        .appendField(new Blockly.FieldNumber(5, -2, 8, 1), "OCTAVE");
+    this.setOutput(true, "note");
+    this.setColour(135);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['live_loop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("loop")
+        .appendField(new Blockly.FieldTextInput("loop_name"), "LOOP_NAME");
+    this.appendStatementInput("STATEMENT")
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(45);
+    this.setTooltip('');
+
+  }
+};
+
+Blockly.Blocks['sleep'] = {
+  init: function() {
+    this.appendValueInput("SLEEP_PERIOD")
+        .setCheck("duration")
+        .appendField("sleep");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(45);
+    this.setTooltip('');
+
+  }
+};
+
+Blockly.Blocks['bpm'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("set tempo to ")
+        .appendField(new Blockly.FieldNumber(0, 20, 500), "BPM")
+        .appendField("bpm");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(165);
+    this.setTooltip('');
+
+  }
+};
+
+Blockly.Blocks['synth'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("use synth")
+        .appendField(new Blockly.FieldDropdown([["sine", ":sine"], ["saw", ":saw"], ["tb303", ":tb303"], ["square", ":square"]]), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(165);
+    this.setTooltip('');
+
+  }
+};
+
+Blockly.Blocks['fx'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("add effect")
+        .appendField(new Blockly.FieldDropdown([["reverb", ":reverb"], ["echo", ":echo"]]), "FX_NAME");
+    this.appendStatementInput("NAME")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(165);
+    this.setTooltip('');
+
+  }
+};
+
+Blockly.Blocks['play_advanced'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("play");
+    this.appendValueInput("NOTE")
+        .setCheck(["note", "midi-note"]);
+    this.appendValueInput("DURATION")
+        .setCheck("duration")
+        .appendField("for");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+   }
+};
+
+Blockly.Blocks['duration'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("one")
+        .appendField(new Blockly.FieldDropdown([["whole", "1"], ["half", "0.5"], ["quarter", "0.25"]]), "NOTE_BASE")
+        .appendField("note, ")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "IS_DOTTED")
+        .appendField("dotted");
+    this.setOutput(true, "duration");
+    this.setColour(150);
+  }
+};
