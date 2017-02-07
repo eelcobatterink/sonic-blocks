45c9d6fbdbd2e4f409fd40965a2645bcb93d5245
Paul Blid <paul@blid.me>
2017-02-07 02:42:57 +0000

    Bridge now stopping properly on shutdown.

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-	bridge.close();
+	bridge.stop();
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
