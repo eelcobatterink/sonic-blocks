48a2cf4144b2f9bee889d0ee92fa3f8fdbb083b2
Biko Agozino <ba325@cam.ac.uk>
2017-02-24 10:55:25 +0000

    Added toolbox.xml for future import Changed in_thread to allow previous and next statements. Added example to the toolbox

--- ui/js/sonicpi_blocks.js
+++ ui/js/sonicpi_blocks.js
@@ -180,6 +180,8 @@ Blockly.Blocks['in_thread'] = {
         this.setColour(230);
         this.setTooltip('');
         this.setHelpUrl('');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
     }
 };
 
