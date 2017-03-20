44b682962e5810732e033c5a483598894cfe47da
Liem Phan <liem.t.phan@tsgforce.com>
2017-03-20 09:04:16 +0000

    Use chai's expect style assertions.

--- test/sonicpi_test.js
+++ test/sonicpi_test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const Blockly = require('./blockly');
 
 let generateCode = (xmlText, workspace) => {
@@ -23,19 +23,19 @@ describe('Code generation', function() {
     it('should return default note value when no input is present', function() {
       xmlText = `<xml><block type="play_basic"></block></xml>`;
       code = generateCode(xmlText, this.workspace);
-      assert.equal(code, "play 72\n");
+      expect(code).to.equal("play 72\n");
     });
 
     it('should play midi_note', function() {
       xmlText = `<xml><block type="play_basic"><value name="NOTE"><block type="midi_note"><field name="NOTE_NUMBER">65</field></block></value></block></xml>`;
       code = generateCode(xmlText, this.workspace);
-      assert.equal(code, "play 65\n");
+      expect(code).to.equal("play 65\n");
     });
 
     it('should play note', function() {
       xmlText = `<xml><block type="play_basic"><value name="NOTE"><block type="note"><field name="NOTE">:C</field><field name="OCTAVE">5</field></block></value></block></xml>`;
       code = generateCode(xmlText, this.workspace);
-      assert.equal(code, "play :C5\n");
+      expect(code).to.equal("play :C5\n");
     });
   });
 });
