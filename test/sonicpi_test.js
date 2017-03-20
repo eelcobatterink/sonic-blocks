4d83ee772ddf20096b8c02bda4e9cdb548361932
Liem Phan <liem.t.phan@tsgforce.com>
2017-03-20 08:52:41 +0000

    Added example play block code generation tests.
    * Refactor to reuse the same workspace before all tests in 'Code generation' tests.
    * That workspace is cleared and disposed after each test.

--- test/sonicpi_test.js
+++ test/sonicpi_test.js
@@ -1,15 +1,41 @@
 const assert = require('assert');
 const Blockly = require('./blockly');
 
+let generateCode = (xmlText, workspace) => {
+  xml = Blockly.Xml.textToDom(xmlText);
+  Blockly.Xml.domToWorkspace(xml, workspace);
+  code = Blockly.SonicPi.workspaceToCode(workspace);
+  return code;
+}
+
 describe('Code generation', function() {
+
+  before(function() {
+    this.workspace = new Blockly.Workspace();
+  });
+
+  afterEach(function() {
+    this.workspace.clear();
+    this.workspace.dispose();
+  });
+
   describe('play block', function() {
     it('should return default note value when no input is present', function() {
-      workspace = new Blockly.Workspace();
       xmlText = `<xml><block type="play_basic"></block></xml>`;
-      xml = Blockly.Xml.textToDom(xmlText);
-      Blockly.Xml.domToWorkspace(xml, workspace);
-      code = Blockly.SonicPi.workspaceToCode(workspace);
+      code = generateCode(xmlText, this.workspace);
       assert.equal(code, "play 72\n");
+    });
+
+    it('should play midi_note', function() {
+      xmlText = `<xml><block type="play_basic"><value name="NOTE"><block type="midi_note"><field name="NOTE_NUMBER">65</field></block></value></block></xml>`;
+      code = generateCode(xmlText, this.workspace);
+      assert.equal(code, "play 65\n");
+    });
+
+    it('should play note', function() {
+      xmlText = `<xml><block type="play_basic"><value name="NOTE"><block type="note"><field name="NOTE">:C</field><field name="OCTAVE">5</field></block></value></block></xml>`;
+      code = generateCode(xmlText, this.workspace);
+      assert.equal(code, "play :C5\n");
     });
   });
 });
