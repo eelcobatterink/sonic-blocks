4b061dfa0f0a108d9bcd7831b11d243cbaac687f
Liem Phan <liem.t.phan@tsgforce.com>
2017-03-24 11:03:44 +0000

    Use osc-js instead of osc module

--- bridge.js
+++ bridge.js
@@ -1,100 +1,79 @@
 "use strict";
 
-const osc = require("osc");
+const OSC = require("osc-js");
 const WebSocketServer = require("ws").Server;
 const clients = []
 
 var spi = null;
 var wss = null;
 
+let sendPort = { port: 4557 };
+
 let run = () => {
-    spi = new osc.UDPPort({
-        localAddress: "127.0.0.1",
-        localPort: 4558,
+	spi = new OSC({ plugin: new OSC.DatagramPlugin() });
+	spi.open({port: 4558});
+	
+	spi.on("open", () => {
+		console.log("SPI Ready.");
+	});
 
-        remoteAddress: "127.0.0.1",
-        remotePort: 4557
-    });
+	spi.on("/runs/all-completed", (msg) => {
+		for(var client of clients) {
+			client.send(JSON.stringify({type: "all-runs-completed"}));
+		}
+	});
 
-    spi.on("ready", function() {
-        console.log("SPI Ready.");
-    });
+	spi.on("/log/info", (msg) => {
+		console.log("Message, cmd: " + msg.address + ", args: " + msg.args.join(" "));
+	});
 
-    spi.on("message", function(msg) {
+	wss = new WebSocketServer({ port: 4001 });
 
-        switch(msg.address) {
-            case "/runs/all-completed":
-            for(var client of clients) {
-                client.send(JSON.stringify({type: "all-runs-completed"}));
-            }
-            break;
+	wss.on('connection', function connection(ws) {
+		clients.push(ws);
 
-            default:
-            console.log("Message, cmd: " + msg.address + ", args: " + msg.args.join(" "));
-            break;
-        }
-    });
-    spi.open();
+		ws.on('message', function incoming(message) {
+			let msg = null;
+			try {
+				msg = JSON.parse(message);
+			} catch(err) {
+				console.log("Could not parse websocket message: " + err);
+				return;
+			}
 
+			if(msg.type) {
+				switch(msg.type) {
+					case "run":
+						console.log("Running code:\n" + msg.code);
+						spi.send(new OSC.Message("/run-code", 0, msg.code), sendPort);
+						break;
+					case "stop-all":
+						console.log("Stopping all runs");
+						spi.send(new OSC.Message("/stop-all-jobs", 0), sendPort);
+						break;
+					default:
+						console.log("Received unknown message type " + msg.type);
+						console.log(msg);
+						break;
+				}
+			}
+		});
 
-    wss = new WebSocketServer({ port: 4001 });
-
-    wss.on('connection', function connection(ws) {
-        clients.push(ws);
-
-        ws.on('message', function incoming(message) {
-        let msg = null;
-        try {
-        msg = JSON.parse(message);
-        } catch(err) {
-            console.log("Could not parse websocket message: " + err);
-            return;
-        }
-
-        if(msg.type) {
-            switch(msg.type) {
-                case "run":
-                    console.log("Running code " + msg.code);
-                    spi.send({
-                        address: "/run-code",
-                        args: [0, msg.code]
-                    });
-                break;
-
-                case "stop-all":
-                    console.log("Stopping all runs");
-                    spi.send({
-                        address: "/stop-all-jobs",
-                        args: [0]
-                    });
-                    break;
-
-                default:
-                    console.log("Received unknown message type " + msg.type);
-                    console.log(msg);
-                break;
-                }
-            }
-        });
-
-        ws.on("close", function() {
-            // Remove this websocket from the client array.
-            clients.splice(clients.indexOf(this), 1);
-        });
-    });
+		ws.on("close", function() {
+			// Remove this websocket from the client array.
+			clients.splice(clients.indexOf(this), 1);
+		});
+	});
 }
 
 let stop = () => {
-    // Send a kill message to SPI Server
-    spi.send({
-        address: "/exit",
-        args: [0]
-    });
-    
-    wss.close();
+	// Send a kill message to SPI Server
+	spi.send(new OSC.Message("/exit", 0), sendPort);
+	spi.close();
+	wss.close();
 
-    wss = null;
-    spi = null;
+	wss = null;
+	spi = null;
 }
 
 module.exports = {run: run, stop: stop};
